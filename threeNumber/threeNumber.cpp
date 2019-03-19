6f8bec07118c60c500222c909e910d24f231af74
PARK <mytlsdnkr@gmail.com>
2019-03-19 22:01:02 +0900

    ttt

--- threeNumber/threeNumber.cpp
+++ threeNumber/threeNumber.cpp
@@ -3,14 +3,22 @@ using namespace std;
 
 int main(){
     int num[3];
-    int i;
+    int i,j;
     for(i=0;i<3;i++)
         cin>>num[i];
 
-    int min=num[0];
-    for(i=1;i<3;i++){
-        if(min>num[i])
-            min=num[i];
+    int temp;
+    for(i=0;i<2;i++){
+        for(j=1;j<3;j++){
+            if(num[i]>num[j]){
+                temp=num[j];
+                num[j]=num[i];
+                num[i]=temp;
+            }
+
+        }
     }
+
+    cout<<num[1]<<endl;
 
 }
